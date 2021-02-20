72d927e800280688f97aebf43ab1e14651d61308
Joseph Yu <44690832+JosephZYU@users.noreply.github.com>
2021-02-20 14:16:46 +0800

    Update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -10,7 +10,8 @@ git clone https://github.com/JosephZYU/dotfiles.git
 echo "Downloaded dotfiles from GitHub"
 
 cd dotfiles/
-chmod +x install.sh
+
+# chmod +x install.sh
 
 ./install.sh /home/josephyu
 echo $'\n'$"Completed Personal bash settings!"
