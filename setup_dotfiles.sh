9e456087987d59bb62456258798838d820b061fd
Joseph Yu <44690832+JosephZYU@users.noreply.github.com>
2021-02-20 14:04:42 +0800

    Update and rename bash.sh to setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# jyusys -- A very sinple script to setup on personal settings from GitHub.
+# A very simple script to setup on personal settings from GitHub.
 # By Joseph Yu. (https://github.com/JosephZYU/General Public License version 2.0)
 
 cd ~
@@ -11,5 +11,6 @@ echo "Downloaded dotfiles from GitHub"
 
 cd dotfiles/
 chmod +x install.sh
+
 ./install.sh /home/josephyu
 echo $'\n'$"Completed Personal bash settings!"
