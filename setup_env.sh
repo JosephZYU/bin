5617c338a325698de38c49af2aa8adaac3344dfa
Joseph Yu <44690832+JosephZYU@users.noreply.github.com>
2021-02-20 15:48:40 +0800

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -14,10 +14,12 @@ sudo apt -y install python3-venv
 sudo pip3 install pipenv
 echo $'\n'$"Install Pip and Venv for Ubuntu 20.04"
 
-sudo apt install tree
+# Check for mlocate and install if needed:
+dpkg -l | grep -qw tree || sudo apt install tree -yyq
 echo $'\n'$"Installed tree"
 
-sudo apt install mlocate
+# Check for mlocate and install if needed:
+dpkg -l | grep -qw mlocate || sudo apt install mlocate -yyq
 echo $'\n'$"Installed mlocate to enable locate search"
 
 echo $'\n'$"COMPLETED insatllations for Pip and Venv!"
