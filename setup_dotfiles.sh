0d02e456374330efcc5b75d2164f88644c3996a8
Joseph Yu <44690832+JosephZYU@users.noreply.github.com>
2021-02-21 16:20:30 +0800

    Update setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -11,8 +11,17 @@ git clone https://github.com/JosephZYU/dotfiles.git
 echo "Downloaded dotfiles from GitHub"
 
 cd dotfiles/
-
 # chmod +x install.sh
 
 ./install.sh $HOME
 echo $'\n'$"Completed Personal bash settings!"
+
+# NOTE: Must include the $HOME as an input argument (variable)
+# ./install.sh $HOME
+
+# if [ "$#" -ne 1 ]; then
+#     echo "Usage: install.sh <home_directory>"
+#     exit 1
+# fi
+
+# homedir=$1
